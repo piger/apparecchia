c0e3aa06a7ade853a7e91b55e48914c220f6126d
Daniel Kertesz <daniel@spatof.org>
2017-03-08 20:18:53 +0000

    import code

---
+++ README.md
@@ -0,0 +1,8 @@
+# apparecchia
+
+Quick and dirty provisioning script for a basic system administrator workstation.
+
+## Usage
+
+Just run `./setup.sh` and follow the on screen instructions; `sudo` will ask for your password to install Homebrew.
+If something goes wrong you can re-run this script as many times as you need.
---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+VIRTUALENV="./.virtualenv"
+
+install_cmdline() {
+    echo "Installing command line tools..."
+    if [[ -z $(xcode-select -p) ]]; then
+        xcode-select --install
+        echo "Press return when the installation of the command line tools is complete"
+        read
+    fi
+}
+
+install_homebrew() {
+    echo "Installing homebrew..."
+    if ! which brew >/dev/null 2>&1; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+}
+
+install_virtualenv() {
+    echo "Installing virtualenv (for Ansible)..."
+    if ! which virtualenv >/dev/null 2>&1; then
+        sudo easy_install virtualenv
+    fi
+}
+
+install_ansible() {
+    echo "Installing Ansible inside the virtualenv..."
+    if [[ ! -L ansible-playbook ]]; then
+        virtualenv $VIRTUALENV
+        ${VIRTUALENV}/bin/pip install Ansible
+    fi
+}
+
+cat <<EOF
++-----------------------------------------------------------------------------------------+
+| This script will install some basic software packages: command line tools, virtualenv,  |
+| homebrew and then will create a virtualenv for Ansible.                                 |
+|                                                                                         |
+| Interrupt now pressing CTRL-c or press Enter to proceed.                                |
++-----------------------------------------------------------------------------------------+
+EOF
+read
+
+umask 022
+
+install_cmdline
+install_virtualenv
+install_homebrew
+install_ansible
+echo "Running Ansible to complete the installation..."
+${VIRTUALENV}/bin/ansible-playbook runbook.yml
